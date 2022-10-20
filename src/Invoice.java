7c2e9bae1641476b15ac8cee9fb048f818eb2c9b
Kookl <reonrobb@gmail.com>
2022-10-20 11:37:23 -0500

    commit

---
+++ src/Invoice.java
@@ -0,0 +1,83 @@
+import format.Date;
+
+public class Invoice {
+
+    private int invoiceNo;
+    private Date billingDate;
+    private String item;
+    private int quantity;
+    private String cashierName;
+    private String customerName;
+
+    public Invoice(int invoiceNo, Date billingDate, String item, int quantity, String cashierName, String customerName)
+    {
+        this.invoiceNo = invoiceNo;
+        this.billingDate = billingDate;
+        this.item = item;
+        this.quantity = quantity;
+        this.cashierName = cashierName;
+        this.customerName = customerName;
+    }
+
+    public int getInvoiceNo() {
+        return invoiceNo;
+    }
+
+    public void setInvoiceNo(int invoiceNo) {
+        this.invoiceNo = invoiceNo;
+    }
+
+    public Date getBillingDate() {
+        return billingDate;
+    }
+
+    public void setBillingDate(Date billingDate) {
+        this.billingDate = billingDate;
+    }
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getCashierName() {
+        return cashierName;
+    }
+
+    public void setCashierName(String cashierName) {
+        this.cashierName = cashierName;
+    }
+
+    public String getCustomerName() {
+        return customerName;
+    }
+
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
+    }
+
+    @Override
+    public String toString() {
+        return "Invoice{" +
+                "invoiceNo=" + invoiceNo +
+                ", billingDate=" + billingDate +
+                ", item='" + item + '\'' +
+                ", quantity=" + quantity +
+                ", cashierName='" + cashierName + '\'' +
+                ", customerName='" + customerName + '\'' +
+                '}';
+    }
+
+
+}
