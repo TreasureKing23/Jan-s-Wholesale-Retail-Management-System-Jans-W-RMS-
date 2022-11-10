2fd7ba0ddf5b9556b4b59855e9d74329f9f757ca
Anelka Lopez <anl.ka2543@hotmail.com>
2022-11-10 15:50:14 -0500

    Address broken down to separate inputs
    Co-Authored-By: Reon Robb <71950922+Kookl@users.noreply.github.com>

--- src/gui/Register.java
+++ src/gui/Register.java
@@ -22,13 +22,17 @@ public class Register {
         JLabel label = new JLabel("Register a new customer");
         JLabel nameLabel = new JLabel("Name: ");
         JLabel DobLabel = new JLabel("Date of Birth: ");
-        JLabel AddressLabel = new JLabel("Address: ");
+        JLabel streetLabel = new JLabel("Street: ");
+        JLabel townLabel = new JLabel("Town/City: ");
+        JLabel parishLabel = new JLabel("Parish: ");
         JLabel emailAddressLabel = new JLabel("Email: ");
         JLabel telLabel = new JLabel("Telephone: ");
 
         JTextField nameText = new JTextField();
         JFormattedTextField dobInput = new JFormattedTextField(df);
-        JTextArea addressText = new JTextArea();
+        JTextField streetText = new JTextField();
+        JTextField townText = new JTextField();
+        JTextField parishText = new JTextField();
         JTextField emailText = new JTextField();
         JTextField telText = new JTextField();
 
@@ -39,19 +43,23 @@ public class Register {
         nameText.setBounds(120, 100,200,25);
         DobLabel.setBounds(20, 130,100,25);
         dobInput.setBounds(120, 130,200,25);
-        AddressLabel.setBounds(20, 160,100,25);
-        addressText.setBounds(120, 160,200,60);
-        emailAddressLabel.setBounds(20, 240,100,25);
-        emailText.setBounds(120, 240,200,25);
-        telLabel.setBounds(20, 270,100,25);
-        telText.setBounds(120, 270,200,25);
+        streetLabel.setBounds(20, 160,100,25);
+        streetText.setBounds(120, 160,200,25);
+        townLabel.setBounds(20, 190,100,25);
+        townText.setBounds(120, 190,200,25);
+        parishLabel.setBounds(20, 220,100,25);
+        parishText.setBounds(120, 220,200,25);
+        emailAddressLabel.setBounds(20, 250,100,25);
+        emailText.setBounds(120, 250,200,25);
+        telLabel.setBounds(20, 280,100,25);
+        telText.setBounds(120, 280,200,25);
 
         register.setBounds(200, 400, 100, 25);
 
         register.addActionListener(e -> {
             String name = nameText.getText();
             String dob = dobInput.getText();
-            String address = addressText.getText();
+            //String address = addressText.getText();
             String email = emailText.getText();
             String tel = telText.getText();
             String dateOfMembership = dateTime.format(LocalDateTime.now());
@@ -68,8 +76,12 @@ public class Register {
         rPanel.add(nameText);
         rPanel.add(DobLabel);
         rPanel.add(dobInput);
-        rPanel.add(AddressLabel);
-        rPanel.add(addressText);
+        rPanel.add(streetLabel);
+        rPanel.add(streetText);
+        rPanel.add(townLabel);
+        rPanel.add(townText);
+        rPanel.add(parishLabel);
+        rPanel.add(parishText);
         rPanel.add(emailAddressLabel);
         rPanel.add(emailText);
         rPanel.add(telLabel);
